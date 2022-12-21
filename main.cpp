08b9630acf1628c9bb3b91d21f8425af02a6ff19
LorenzoCaligiuri <115158644+LorenzoCaligiuri@users.noreply.github.com>
2022-12-21 12:37:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,37 @@
 #include <iostream>
+#include <cmath>
+#include <iomanip>
 using namespace std;
 
+double f(double y){
+    return pow(y, 2)* cos(y)+1;
+}
+
 int main() {
-   return 0;
+    int counter=0;
+    double a=0, b=0, c=0, x=0, err=0;
+    do{
+        cout<<"inserire estremi"<<endl;
+        cin>>a>>b;
+    }while(f(a)*f(b)>=0);
+    if(f(a)<f(b)){
+        c=a;
+        a=b;
+        b=c;
+    }
+    do{
+        x=(a+b)/2;
+        if(f(x)==0){
+            break;
+        }else if(f(x)<0){
+            b=x;
+        }else{
+            a=x;
+        }
+        err=abs((b-a)/2);
+        counter++;
+    }while(err>=0.000001);
+    cout<<setprecision(4)<<x<<endl;
+    cout<<counter<<endl;
+    return 0;
 }
